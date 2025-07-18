d92b75f3dfc9ee5f088fb14db87a0fe68b7bdec0
Joel <garciacervantesj00@gmail.com>
2025-07-17 17:06:32 -0700

    updated concepts.md

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import Button from './expirements/button'
-
+import Student from './expirements/Student.jsx'
 function App() {
 
   return (
@@ -7,7 +7,11 @@ function App() {
     //<Header></Header> This is valid,but shorthand way below
     // <></> = "fragement" used to technically return a single element
     <>
-      <Button />
+      <Student name="Spongebob" age={30} isStudent={false} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Sandy" age={35} isStudent={true} />
+      <Student name="Squidward" age={50} isStudent={false} />
+
     </>
 
   );
